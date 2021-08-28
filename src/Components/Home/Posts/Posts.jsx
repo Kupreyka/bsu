4aa2c6080a7474719639b962a00f538e7d19e9f1
Kupreyka <Kupreyka@users.noreply.github.com>
2021-08-28 14:34:24 +0300

    refactoring main page and messages page(add map)

--- src/Components/Home/Posts/Posts.jsx
+++ src/Components/Home/Posts/Posts.jsx
@@ -1,14 +1,20 @@
 import style from "../Home.module.css";
 import Post from "../Post/Post";
 
-const Posts = (props) => {
+const Posts = () => {
+    let messageData = [
+        {message:'Hello'},
+        {message:'How are you?'},
+        {message:'Sorry'}
+    ]
+
+    let messageElement = messageData.map(el => <Post message={el.message}/>)
+
     return(
         <div className={style.post}>
-            <textarea name="" id="" cols="50" rows="10" placeholder='Новый пост'></textarea>
+            <textarea cols="50" rows="10" placeholder='Новый пост'></textarea>
             <button>Add Post</button>
-            <Post message='Hello'/>
-            <Post message='How are you?'/>
-            <Post message='Sorry'/>
+            {messageElement}
         </div>
     )
 }
--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -13,27 +13,34 @@ const Messages = (props) => {
     )
 }
 
-const Message = (props) => {
+const Message = () => {
     let nameData = [
-        {id:1, name:'Kirill'},
-        {id:2, name:'Svetlana'},
-        {id:3, name:'Andrey'},
-        {id:4, name:'Artem'},
-        {id:5, name:'Alexey'}
+        {id: 1, name: 'Kirill'},
+        {id: 2, name: 'Svetlana'},
+        {id: 3, name: 'Andrey'},
+        {id: 4, name: 'Artem'},
+        {id: 5, name: 'Alexey'}
     ]
+
+    let MessageData = [
+        {message: 'Hello'},
+        {message: 'How are you?'},
+        {message: 'Goodbye'}
+    ]
+
+    let MessageElement = MessageData.map(el => <Messages message={el.message}/>)
+
+
+    let nameElement = nameData.map(el => <Name id={el.id} name={el.name}/>);
+
+
     return (
         <div className={style.dialogs}>
             <div className={style.name}>
-                <Name id={nameData[0].id} name={nameData[0].name}/>
-                <Name id={nameData[1].id} name={nameData[1].name}/>
-                <Name id={nameData[2].id} name={nameData[2].name}/>
-                <Name id={nameData[3].id} name={nameData[3].name}/>
-                <Name id={nameData[4].id} name={nameData[4].name}/>
+                {nameElement}
             </div>
             <div className={style.message}>
-                <Messages message='Hello'/>
-                <Messages message='How are you?'/>
-                <Messages message='Sorry'/>
+                {MessageElement}
             </div>
         </div>
     )
